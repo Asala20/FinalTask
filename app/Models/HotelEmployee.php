a0faf17f804ea513c1acf6c8694638b2d765f6bb
hashem othman <hashim993.hel993@emitisgroup.com>
2023-05-25 16:41:12 +0200

    model and migration

---
+++ app/Models/HotelEmployee.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HotelEmployee extends Model
+{
+    use HasFactory;
+}
