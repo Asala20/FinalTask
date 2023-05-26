25ba3eb5cb04c8cf47b99a3c5c16d230a68de46b
Asala20 <108730648+Asala20@users.noreply.github.com>
2023-05-25 18:30:50 -0700

    Auth_Middlware_task

--- database/migrations/2023_05_26_010609_create_hotels_table.php
+++ database/migrations/2023_05_26_010609_create_hotels_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('hotels', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->date('age');
-            $table->string('phone');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('rooms');
+            $table->string('address');
+            $table->integer('stars_num');
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('hotels');
     }
 };
